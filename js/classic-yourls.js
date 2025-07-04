e78299f368fea179e98e10eb4d9e8f91463ce8b6
Graham McKoen <graham55@users.noreply.github.com>
2025-07-04 20:04:57 +0100

    Update and rename better-yourls.js to classic-yourls.js
    change references from better to classic

---
+++ js/classic-yourls.js
@@ -0,0 +1,9 @@
+jQuery( document ).ready( function () {
+
+	jQuery( '#wp-admin-bar-classic_yourls-link' ).click( function () {
+
+		alert( classic_yourls.text + '\n\n' + classic_yourls.yourls_url );
+
+	} );
+
+} );
